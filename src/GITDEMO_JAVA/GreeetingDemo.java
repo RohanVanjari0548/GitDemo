bbb1b203f8f1222300ca111e755df038ff679638
RohanVanjari0548 <153060729+RohanVanjari0548@users.noreply.github.com>
2023-12-06 18:20:19 +0530

    Update GreeetingDemo.java

--- src/GITDEMO_JAVA/GreeetingDemo.java
+++ src/GITDEMO_JAVA/GreeetingDemo.java
@@ -5,7 +5,7 @@ public class GreeetingDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT");
-		System.out.println("This is change done by other developer");
+		System.out.println("This is change done by other developer 123");
 		System.out.println("Okay");
 
 	}
