ea38c6536df1b19348a4dbde6cb5b4a4dcf7f3fd
rohan.vanjari0548 <rohanvanjari@gmail.com>
2023-12-06 12:45:50 +0530

    1. printed 'Hi' on console

---
+++ src/GITDEMO_JAVA/GreeetingDemo.java
@@ -0,0 +1,11 @@
+package GITDEMO_JAVA;
+
+public class GreeetingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GIT");
+
+	}
+
+}
